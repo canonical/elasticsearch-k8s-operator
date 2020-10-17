73b09ba457ba1e9708371074fdee87e562941cad
justin <justinmauriceclark@gmail.com>
2020-10-17 17:32:27 -0400

    support dynamic cluster-wide settings without pod reset
    This commit adds the functionality for Elasticsearch to have cluster wide settings updated via the REST API to avoid having to do pod resets. As of this commit, there is only 1 setting: discovery.zen.minimum_master_nodes. For ES version 6.8, this is one of the important settings that administrators must consider. Adding new settings should be trivial.
    
    This is necessary because resetting pods for ES can lead to much unnecessary shard shuffling.
    
    Updating dynamic settings is similar to configuring the Juju pod spec, so dynamic configuration follows a similar pattern: 1. a `_configure_dynamic_settings()` function to make the API call, 2. a `_build_dynamic_settings_payload()` function to create the necessary data structure to pass via the API, 3. getter functions that are called within the payload creation function.

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -1,6 +1,7 @@
 # Copyright 2020 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import random
 import unittest
 import yaml
 
@@ -56,6 +57,56 @@ class TestCharm(unittest.TestCase):
         seed_hosts_file = config_file(pod_spec, 'unicast_hosts.txt')
         self.assertEqual(charm.SEED_SIZE, 4)
         self.assertEqual(charm.SEED_SIZE, len(seed_hosts_file['content'].split("\n")))
+
+    def test_num_hosts_is_equal_to_num_units(self):
+        self.harness.set_leader(True)
+        seed_config = MINIMAL_CONFIG.copy()
+        self.harness.update_config(seed_config)
+
+        # add a random number of peer units
+        rel_id = self.harness.add_relation('elasticsearch', 'elasticsearch')
+        self.assertIsInstance(rel_id, int)
+        num_units = random.randint(2, 10)
+
+        # elasticsearch-operator/0 already exists as the starting unit
+        for i in range(1, num_units):
+            self.harness.add_relation_unit(rel_id, 'elasticsearch-operator/{}'.format(i))
+        self.assertEqual(self.harness.charm.num_hosts, num_units)
+
+    def test_minimum_master_nodes_matches_formula(self):
+        # Test whether _minimum_master_nodes function
+        # matches formula N / 2 + 1 for N > 2, 1 otherwise
+        self.harness.set_leader(True)
+        seed_config = MINIMAL_CONFIG.copy()
+        self.harness.update_config(seed_config)
+        rel_id = self.harness.add_relation('elasticsearch', 'elasticsearch')
+
+        # create inputs of three main case categories with two examples each
+        # case categories: num_nodes <= 2, num_nodes is even, num_nodes is odd
+        total_mmn_cases = [(1, 1), (2, 1), (4, 3), (5, 3), (6, 4), (7, 4)]
+        for (num_nodes, expected_mmn) in total_mmn_cases:
+            with self.subTest():
+                for i in range(1, num_nodes):
+                    self.harness.add_relation_unit(rel_id, 'elasticsearch-operator/{}'.format(i))
+                actual_mmn = self.harness.charm._min_master_nodes()
+                self.assertEqual(expected_mmn, actual_mmn)
+
+    def test_dynamic_settings_payload_has_correct_minimum_master_nodes(self):
+        self.harness.set_leader(True)
+        seed_config = MINIMAL_CONFIG.copy()
+        self.harness.update_config(seed_config)
+
+        # create the peer relation
+        rel_id = self.harness.add_relation('elasticsearch', 'elasticsearch')
+
+        # when number of nodes is 6, min master nodes should be 6 / 2 + 1 = 4
+        num_nodes = 6
+        expected_mmn = 4
+        for i in range(1, num_nodes):
+            self.harness.add_relation_unit(rel_id, 'elasticsearch-operator/{}'.format(i))
+        payload = self.harness.charm._build_dynamic_settings_payload()
+        actual_mmn = payload['persistent']['discovery.zen.minimum_master_nodes']
+        self.assertEqual(expected_mmn, actual_mmn)
 
 
 def config_file(pod_spec, file):
